ed117963eff393ba7970f7916f674a391240988d
Pauli Niva <Pauli@Vulcan-3.local>
2014-12-30 17:43:42 +0200

    Valosensori lisatty
    Valosensorin kalibrointi ja perustoiminnot

---
+++ src/sensorit/Valosensori.java
@@ -0,0 +1,63 @@
+package sensorit;
+
+import lejos.nxt.Button;
+import lejos.nxt.LCD;
+import lejos.nxt.LightSensor;
+import lejos.nxt.SensorPort;
+
+/**
+ * Sensori joka erottaa tumman viivan vaaleammasta taustasta
+ * 
+ * Valoarvot (viivan arvo ja taustan arvo) kalibroidaan valosensorilla ja
+ * kynnysarvo lasketaan naiden lukujen mediaanina. Kynnysarvoa käytetaan viivan
+ * ja taustan erottamiseen.
+ * 
+ * @author Pauli Niva
+ * @version 30122014
+ */
+
+public class Valosensori {
+
+    private LightSensor valosensori;
+    private int viivanArvo;
+    private int taustanArvo;
+    private int kynnysarvo;
+
+    public Valosensori(SensorPort sensoriportti) {
+	valosensori = new LightSensor(sensoriportti);
+	valosensori.setFloodlight(true);
+    }
+
+    @SuppressWarnings("deprecation")
+    private int lueArvo(String kohde) {
+	int sensorinArvo = 0;
+	LCD.clear();
+	LCD.drawString("PAINA ORANSSIA", 0, 0);
+	LCD.drawString("KALIBROIDAKSESI", 0, 1);
+	LCD.drawString("kohde", 0, 2);
+	while (!Button.ENTER.isPressed()) {
+	    sensorinArvo = valosensori.readValue();
+	    LCD.drawInt(sensorinArvo, 4, 5, 2);
+	    LCD.refresh();
+	}
+	return sensorinArvo;
+    }
+
+    public void kalibroi() {
+	this.viivanArvo = lueArvo("VIIVA");
+	this.taustanArvo = lueArvo("TAUSTA");
+	this.kynnysarvo = (viivanArvo + taustanArvo) / 2;
+    }
+
+    public int getViivanArvo() {
+	return this.viivanArvo;
+    }
+
+    public int getTaustanArvo() {
+	return this.taustanArvo;
+    }
+
+    public int getKynnysarvo() {
+	return this.kynnysarvo;
+    }
+}
